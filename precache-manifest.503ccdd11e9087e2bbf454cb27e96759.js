ff89b0ee802bd77ab81b77bca125e517d38694c1
Pawel <ocebeki@gmail.com>
2020-07-01 14:53:29 +0200

    Updates

---
+++ precache-manifest.503ccdd11e9087e2bbf454cb27e96759.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a6229ce2e38ed3bdf386c00528d8fd",
+    "url": "/flight-search-app/index.html"
+  },
+  {
+    "revision": "a2ad8d46e220c83a7e41",
+    "url": "/flight-search-app/static/css/main.d656f6e1.chunk.css"
+  },
+  {
+    "revision": "6834b5c4078aaaf253fd",
+    "url": "/flight-search-app/static/js/2.07ae15ff.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/flight-search-app/static/js/2.07ae15ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ad8d46e220c83a7e41",
+    "url": "/flight-search-app/static/js/main.55000a39.chunk.js"
+  },
+  {
+    "revision": "653a34e5964540f190f7",
+    "url": "/flight-search-app/static/js/runtime-main.5a7e9639.js"
+  },
+  {
+    "revision": "17756d2da3db62f61d40361a94007b8b",
+    "url": "/flight-search-app/static/media/airplane.17756d2d.svg"
+  },
+  {
+    "revision": "ce9083e7eaffd674e9f9f82b5f2da1ff",
+    "url": "/flight-search-app/static/media/button.ce9083e7.svg"
+  },
+  {
+    "revision": "06412bd8ae8f9110994a56e33b4f8c85",
+    "url": "/flight-search-app/static/media/clock.06412bd8.svg"
+  }
+]);
